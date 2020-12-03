5a8ae86995037f2d2aaac62b30a5867aaf615d6c
Oskar Petersen <oskar.petersen@trioptima.com>
2020-12-03 16:02:27 +0100

    Day 3 clean-up

--- 03/solution.py
+++ 03/solution.py
@@ -5,23 +5,20 @@ strategies = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
 width = len(m[0])
 product_sum = 1
-for s in strategies:
-    dx = s[0]
-    dy = s[1]
-    xpos = 0
-    ypos = 0
-    sum_trees = 0
 
-    while ypos < len(m):
-        row = m[ypos]
-        if row[xpos] == '#':
+for dx, dy in strategies:
+    x = y = sum_trees = 0
+
+    while y < len(m):
+        row = m[y]
+        if row[x] == '#':
             sum_trees += 1
-        xpos = (xpos+dx) % width
-        ypos += dy
+        x = (x+dx) % width
+        y += dy
 
     product_sum *= sum_trees
     
-    if s == (3,1):
+    if (dx,dy) == (3,1):
         print("Part 1: " + str(sum_trees))
 
 print('Part 2: ' + str(product_sum))
