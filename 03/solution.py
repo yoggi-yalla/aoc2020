e9732e8068f3f3d9ab3abd11c1d619e310eb58af
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2020-12-06 07:57:04 +0100

    clean-up

--- 03/solution.py
+++ 03/solution.py
@@ -1,16 +1,17 @@
 with open('input.txt','r') as f:
-    m = f.read().split('\n')
+    forest = f.read().split('\n')
 
 strategies = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
-width = len(m[0])
-product_sum = 1
+height = len(forest)
+width = len(forest[0])
 
+product_sum = 1
 for dx, dy in strategies:
+
     x = y = sum_trees = 0
-    while y < len(m):
-        row = m[y]
-        if row[x] == '#':
+    while y < height:
+        if forest[y][x] == '#':
             sum_trees += 1
         x = (x+dx) % width
         y += dy
