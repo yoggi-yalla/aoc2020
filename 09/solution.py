c1ad431b29fd98af65c21a0ac78213e28d8295f2
Oskar Petersen <oskar.petersen@trioptima.com>
2020-12-09 09:52:40 +0100

    Day 9

--- 09/solution.py
+++ 09/solution.py
@@ -1,29 +1,29 @@
 from itertools import combinations
 
 def two_sums_to_k(k, cache):
-    for a,b in combinations(cache,2):
-        if a+b == k:
+    for a, b in combinations(cache, 2):
+        if a + b == k:
             return True
     return False
 
 def find_first_invalid(ints, cache_size):
     for i in range(cache_size, len(ints)):
-        cache = ints[i-cache_size:i]
+        cache = ints[i - cache_size : i]
         if not two_sums_to_k(ints[i], cache):
             return ints[i]
 
-def find_encryption_weakness(ints, invalid):
-    for i in range(len(ints)):
-        for j in range(i+2,len(ints)):
-            if sum(ints[i:j]) > invalid:
-                break
-            if sum(ints[i:j]) == invalid:
-                return max(ints[i:j]) + min(ints[i:j])
+def find_encryption_weakness(ints, target):
+    i, j = 0, 1
+    while j < len(ints):
+        if sum(ints[i:j]) < target:
+            j += 1
+        elif sum(ints[i:j]) > target:
+            i += 1
+        else:
+            return max(ints[i:j]) + min(ints[i:j])
 
 with open('input.txt', 'r') as f:
-    data = f.read()
-    
-ints = [int(x) for x in data.split('\n')]
+    ints = [int(x) for x in f.read().split('\n')]
 
 part1 = find_first_invalid(ints, 25)
 part2 = find_encryption_weakness(ints, part1)
