d92a56097fb81cbdb8aa69b668e0b7c7b2f07b84
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2020-12-12 11:03:26 +0100

    Day 11 clean-up

--- 11/solution.py
+++ 11/solution.py
@@ -1,105 +1,95 @@
-def part1(grid, seats, seat_to_neighbors):
-    while 1:
-        changed = set()
-        for i,j in seats:
-            count = 0
-            for k,l in seat_to_neighbors[i,j]:
-                v = "#"
-                if (k,l) in changed:
-                    v = "L"
-                if grid[k][l] == v:
-                    count += 1
-            if grid[i][j] == "#" and count >=4:
-                grid[i][j] = "L"
-                changed.add((i,j))
+def compute_shifts(grid, tolerance, surroundings_map):
+    buffer = {}
+    for i in range(len(grid)):
+        for j in range(len(grid[i])): 
+            if grid[i][j] == ".":
+                continue
+            count = count_surroundings(i, j, grid, surroundings_map)
+            if grid[i][j] == "#" and count >= tolerance:
+                buffer[i,j] = "L"
             elif grid[i][j] == "L" and count == 0:
-                grid[i][j] = "#"
-                changed.add((i,j))
-        if not changed:
-            break
+                buffer[i,j] = "#"
+    return buffer
+
+def count_surroundings(i, j, grid, surroundings_map):
     count = 0
-    for i,j in seats:
-        if grid[i][j] == "#":
+    for i_n, j_n in surroundings_map[i,j]:
+        if grid[i_n][j_n] == "#":
             count += 1
     return count
 
-def part2(grid, seats, seat_to_visible):
-    while 1:
-        changed = set()
-        for i,j in seats:
-            count = 0
-            for k,l in seat_to_visible[i,j]:
-                v = "#"
-                if (k,l) in changed:
-                    v = "L"
-                if grid[k][l] == v:
-                    count += 1
-            if grid[i][j] == "#" and count >=5:
-                grid[i][j] = "L"
-                changed.add((i,j))
-            elif grid[i][j] == "L" and count == 0:
-                grid[i][j] = "#"
-                changed.add((i,j))
-        if not changed:
-            break
+def update_grid(grid, buffer):
+    for (i,j), v in buffer.items():
+        grid[i][j] = v
+
+def count_seated(grid):
     count = 0
-    for i,j in seats:
-        if grid[i][j] == "#":
-            count += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "#":
+                count += 1
     return count
+
+def run(grid, tolerance, surroundings):
+    while 1:
+        buffer = compute_shifts(grid, tolerance, surroundings)
+        update_grid(grid, buffer)
+        if not buffer:
+            break
+    return count_seated(grid)
     
-with open('input.txt', 'r') as f:
-    data = f.read()
+def main():
+    with open('input.txt', 'r') as f:
+        data = f.read()
 
-grid = [list(row) for row in data.split('\n')]
+    grid = [list(row) for row in data.split('\n')]
+    
+    height = len(grid)
+    width = len(grid[0])
 
-height = len(grid)
-width = len(grid[0])
+    directions = (
+        (-1,-1), 
+        (-1, 0), 
+        (-1, 1), 
+        ( 0, 1), 
+        ( 0,-1), 
+        ( 1,-1), 
+        ( 1, 0), 
+        ( 1, 1)
+    )
 
-directions = (
-    (-1,-1), 
-    (-1, 0), 
-    (-1, 1), 
-    ( 0, 1), 
-    ( 0,-1), 
-    ( 1,-1), 
-    ( 1, 0), 
-    ( 1, 1)
-)
+    seats = set()
+    seat_to_neighbors = {}
+    seat_to_visible = {}
 
-coords = set()
-seats = set()
-seat_to_neighbors = {}
-seat_to_visible = {}
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] != ".":
+                seats.add((i,j))
+                seat_to_neighbors[i,j] = set()
+                seat_to_visible[i,j] = set()
 
-for i in range(height):
-    for j in range(width):
-        coords.add((i,j))
-        if grid[i][j] != ".":
-            seats.add((i,j))
-            seat_to_neighbors[i,j] = set()
-            seat_to_visible[i,j] = set()
+    for i, j in seats:
+        for d_i, d_j in directions:
+            i_n = i + d_i
+            j_n = j + d_j
+            if (i_n, j_n) in seats:
+                seat_to_neighbors[i,j].add((i_n,j_n))
+            while 1:
+                if not ((0 <= i_n < height) and (0 <= j_n < width)):
+                    break
+                if grid[i_n][j_n] != ".":
+                    seat_to_visible[i,j].add((i_n,j_n))
+                    break
+                i_n += d_i
+                j_n += d_j
 
-for i, j in seats:
-    for d_i, d_j in directions:
-        k = i + d_i
-        l = j + d_j
-        if (k,l) in seats:
-            seat_to_neighbors[i,j].add((k,l))
-        while 1:
-            if (k,l) not in coords:
-                break
-            if grid[k][l] != ".":
-                seat_to_visible[i,j].add((k,l))
-                break
-            k += d_i
-            l += d_j
+    grid2 = [list(x) for x in grid]
 
+    part_1 = run(grid, 4, seat_to_neighbors)
+    part_2 = run(grid2, 5, seat_to_visible)
 
-grid2 = [list(x) for x in grid]
+    print("Part 1: ", part_1) #2281
+    print("Part 2: ", part_2) #2085
 
-part_1 = part1(grid, seats, seat_to_neighbors)
-part_2 = part2(grid2, seats, seat_to_visible)
-
-print("Part 1: ", part_1) #2281
-print("Part 2: ", part_2) #2085
+main()
