8c3594a0593eddab88f11590d5ca598854144ec8
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-04-04 19:10:42 +0200

    15-19

--- 19/solution.py
+++ 19/solution.py
@@ -1,13 +1,14 @@
 import re
 
 def parse_rule(r):
+    """
+    example input:    "50: 15 65 | 60 74"
+    example output:   {'50':[('15','65'),('60','74')]}
+    """
     rule = {}
     k,v = r.split(':')
     v = v.replace('\"','')
-    value = []
-    alternatives = v.split('|')
-    for alt in alternatives:
-        value.append(tuple(alt.split()))
+    value = [tuple(alt.split()) for alt in v.split('|')]
     rule[k] = value
     return rule
 
@@ -15,19 +16,15 @@ def get_regex_for_rule(r, rules):
     if r not in rules:
         return r
     regex = ""
-    for i in range(len(rules[r])):
-        for k in rules[r][i]:
-            regex += "(" + get_regex_for_rule(k, rules) + ")"
+    for alternative in rules[r]:
+        for v in alternative:
+            regex += "(" + get_regex_for_rule(v, rules) + ")"
         regex += "|"
     return regex[:-1]
 
 def count_valid_messages(messages, rules):
     pattern = re.compile("^" + get_regex_for_rule('0', rules) + "$")
-    count = 0
-    for m in messages:
-        if re.match(pattern, m):
-            count += 1
-    return count
+    return sum(1 for m in messages if re.match(pattern,m))
 
 with open('input.txt', 'r') as f:
     data = f.read()
@@ -40,18 +37,17 @@ with open('input.txt', 'r') as f:
     messages = messages_.split('\n')
 
 
+new_rule_8 = "8: 42"
+for i in range(2,6):
+    new_rule_8 += " |" + " 42"*i
+
+new_rule_11 = "11: 42 31"
+for i in range(2,6):
+    new_rule_11 += " |" + " 42"*i + " 31"*i
+
 updated_rules = rules.copy()
-
-rule_8 = "8: 42"
-for i in range(2,6):
-    rule_8 += " |" + " 42"*i
-
-rule_11 = "11: 42 31"
-for i in range(2,6):
-    rule_11 += " |" + " 42"*i + " 31"*i
-
-updated_rules |= parse_rule(rule_8)
-updated_rules |= parse_rule(rule_11)
+updated_rules |= parse_rule(new_rule_8)
+updated_rules |= parse_rule(new_rule_11)
 
 print("Part 1:", count_valid_messages(messages, rules)) # 171
 print("Part 2:", count_valid_messages(messages, updated_rules)) # 369
